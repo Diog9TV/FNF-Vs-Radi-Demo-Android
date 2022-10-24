df4c2f4f94b57d4f61fc49d06246a26f806bdbcc
Diog9TV <89103529+Diog9TV@users.noreply.github.com>
2022-10-24 19:18:33 -0300

    Add files via upload

---
+++ assets/preload/stages/radiBG.lua
@@ -0,0 +1,117 @@
+local xx = 1350;
+local yy = 1000;
+local xx2 = 1750;
+local yy2 = 1050;
+local ofs = 35;
+local followchars = true;
+local del = 1;
+local del2 = 1;
+
+
+function onCreate()
+    makeLuaSprite('background', 'radi/back-0', 0, 0)
+
+    makeLuaSprite('background-1', 'radi/back-1', -150, -100)
+    setLuaSpriteScrollFactor('background-1', 0.8, 0.8)
+
+    makeLuaSprite('rubble', 'radi/middle', 0, 0)
+
+    makeLuaSprite('front', 'radi/Front', 75, 75)
+    setLuaSpriteScrollFactor('front', 1.1, 1.1)
+
+    addLuaSprite('background', false)
+    addLuaSprite('background-1', false)
+    addLuaSprite('rubble', false)
+    addLuaSprite('front', true)
+
+end
+function onCreatePost()
+
+   --makeLuaTexts
+   makeLuaText('Watermark', 'Port By Thepotra', 0, 15, getProperty('healthBarBG.y') + 30);
+
+  
+   --addLuaText
+   addLuaText('Watermark')
+
+
+   --Text fonts
+   setTextFont('scoreTxt', 'PhantomMuff Full Letters 1.1.5.ttf')
+   setTextFont('healthCounter', 'PhantomMuff Full Letters 1.1.5.ttf')
+   setTextFont('timeTxt', 'PhantomMuff Full Letters 1.1.5.ttf')
+   setTextFont('botplayTxt', 'PhantomMuff Full Letters 1.1.5.ttf')
+   setTextFont('judgementCounter', 'PhantomMuff Full Letters 1.1.5.ttf')
+   setTextFont('Watermark', 'PhantomMuff Full Letters 1.1.5.ttf');
+
+   
+   --Text sizes
+   setTextSize('Watermark', 20);
+   setTextSize('scoreTxt', 20);
+   setTextSize('healthCounter', 20);
+   setTextSize('timeTxt', 20);
+   setTextSize('botplayTxt', 34);
+   setTextSize('judgementCounter', 20);
+
+end
+function onUpdate()
+	if del > 0 then
+		del = del - 1
+	end
+	if del2 > 0 then
+		del2 = del2 - 1
+	end
+    if followchars == true then
+        if mustHitSection == false then
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+	    if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end
+    
+end
